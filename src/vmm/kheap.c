24f5b6deffc432ec1cf8638c1b262ed94925391d
Ariel Ben Eliezer <dodibe2102@gmail.com>
2023-01-06 21:01:26 +0200

    added kmalloc aligned

--- src/vmm/kheap.c
+++ src/vmm/kheap.c
@@ -16,7 +16,7 @@ kheap_metadata_t setup_kheap(e820_mmap_t *mmap, uint64_t max_size)
         e820_mmap_entry_t entry = mmap->entries[mmap_entry_index];
         if (entry.type == MMAP_AVAILABLE && entry.length >= max_size)
         {
-            kheap.end = PAGE_ALIGNED(entry.base + 0x1000); // avoid unavailable pages
+            kheap.end = ALIGNED_UP(entry.base + 1, 0x1000); // avoid unavailable pages
             break;
         }
     }
@@ -29,4 +29,10 @@ void *kmalloc(kheap_metadata_t *kheap, uint64_t size)
     void *address = (void *)kheap->end;
     kheap->end += size;
     return address;
+}
+
+void *kmalloc_aligned(kheap_metadata_t *kheap, uint64_t size, uint64_t align)
+{
+    kheap->end = ALIGNED_UP(kheap->end, align);
+    return kmalloc(kheap, ALIGNED_UP(size, align));
 }
